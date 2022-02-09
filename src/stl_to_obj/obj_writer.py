403c207b474e110e44a4b6963c6798a01c1f3ad3
shivamkadukar <78019141+shivamkadukar@users.noreply.github.com>
2022-02-09 13:13:58 +0530

    Update obj_writer.py

--- src/stl_to_obj/obj_writer.py
+++ src/stl_to_obj/obj_writer.py
@@ -1,23 +1,40 @@
+from utilis.progressbar import printprogreesbar
+
 class ObjWriter():
     def write(self, file_to_write, unique_vertices_list, unique_normals_list, v_index, vn_index):
         
-        for vertex in unique_vertices_list:
+        lv = len(unique_vertices_list)
+        printprogreesbar(0, lv, prefix = 'Converting Vertices:', suffix = 'Complete', length = 50)
+        
+        for i, vertex in enumerate(unique_vertices_list):
             file_to_write.write("v ")
             file_to_write.write(vertex)
             file_to_write.write("\n")
+            
+            printprogreesbar(i + 1, lv, prefix = 'Converting Vertices:', suffix = 'Complete', length = 50)
 
-        for normal in unique_normals_list:
+        lvn = len(unique_normals_list)
+        printprogreesbar(0, lvn, prefix = 'Converting Normals:', suffix = 'Complete', length = 50)
+        
+        for i, normal in enumerate(unique_normals_list):
             file_to_write.write("vn ")
             file_to_write.write(normal)
             file_to_write.write("\n")
+            
+            printprogreesbar(i + 1, lvn, prefix = 'Converting Normals:', suffix = 'Complete', length = 50)
 
         start, end = 0, 3
-        for vn in vn_index:
+        lf = len(vn_index)
+        printprogreesbar(0, lf, prefix = 'Converting Faces:', suffix = 'Complete', length = 50)
+        
+        for i, vn in enumerate(vn_index):
             file_to_write.write('f ')
             for v in v_index[start: end]:
                 file_to_write.write(f'{v}/{vn} ')
             file_to_write.write('\n')
             start += 3
             end += 3
+            
+            printprogreesbar(i+1, lf, prefix = 'Converting Faces:', suffix = 'Complete', length = 50)
 
         
